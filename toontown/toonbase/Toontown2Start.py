87da3e99bb9919915a3e9f81969c0b629e58cc5e
thestormcoder <alessandroabreujunior@gmail.com>
2016-06-03 15:01:18 -0300

    Add files via upload

--- toontown/toonbase/Toontown2Start.py
+++ toontown/toonbase/Toontown2Start.py
@@ -1,4 +1,4 @@
-import __builtin__
+import __builtin__, os
 
 from panda3d.core import loadPrcFile
 loadPrcFile("config/dev.prc")
@@ -16,15 +16,20 @@ __builtin__.loader = base.loader
 try:
     base.createToonBase()
 except:
-    raise Exception('Could not init createToonBase!')
+    raise #Exception('Could not init createToonBase!')
 
 from toontown.distributed.ToontownClientRepository import ToontownClientRepository
 serverVersion = config.GetString('server-version')
 
+if os.environ.has_key("GAMESERVER"):
+    gameserver = os.environ['GAMESERVER']
+else:
+    gameserver = config.GetString('game-server', '127.0.0.1')
+
 __builtin__.base.cr = ToontownClientRepository(
     dcFileNames=['config/toon.dc'], 
     serverVersion=serverVersion, 
-    serverList=[config.GetString('game-server', '127.0.0.1'), 7198], # Port stays default.
+    serverList=[gameserver, 7198], # Port stays default.
     playToken=config.GetString('play-token')
 )
 
