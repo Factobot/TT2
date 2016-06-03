61634bb8e09e8380ac25f2fa1014b17b026a2b39
FerverExtreme <juniorferverextreme@gmail.com>
2016-06-03 15:11:08 -0300

    Dubito's server

---
+++ play_dubito.py
@@ -0,0 +1,4 @@
+import os
+os.environ["GAMESERVER"] = "188.165.250.225"
+
+import main
---
+++ run_dubito.bat
@@ -0,0 +1,5 @@
+@echo off
+set PPYTHON_PATH=C:\Panda3D-1.8.1\python\ppython
+
+%PPYTHON_PATH% play_dubito.py
+pause
