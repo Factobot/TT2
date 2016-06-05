d1720d472b7ff9c67ba180bbfc7dadeb2660f081
SkippsDev <anythingtechpro@gmail.com>
2016-06-04 22:57:47 -0400

    Added camera zooming, and added a keyboard shortcut to reset your camera

--- toontown/controls/InteractiveCameraDriver.py
+++ toontown/controls/InteractiveCameraDriver.py
@@ -1,10 +1,13 @@
 from panda3d.core import *
 from direct.showbase.DirectObject import *
-
+from direct.task.Task import Task
 CameraFactor = 0.3
 
 class InteractiveCameraDriver(DirectObject):
     PITCH_LIMIT = 10
+    MAX_CAM_DISTANCE = 8.5
+    MIN_CAM_DISTANCE = 0
+
     def __init__(self, camera, cameraColl = True):
         self.camera = camera
         self.cameraCollisions = cameraColl
@@ -12,51 +15,95 @@ class InteractiveCameraDriver(DirectObject):
         self.mouseCoords = [0,0]
         self.target = None
         self.wantControl = False
+        self.canControlCam = False
+        self.canZoom = False
         
     def setTarget(self, target):
         self.target = target
         if self.target:
-            self.cameraParent = self.target.attachNewNode("cameraParent")
+            self.cameraParent = self.target.attachNewNode('cameraParent')
             self.camera.reparentTo(self.cameraParent)
             self.camera.lookAt(self.cameraParent)
         
     def start(self):
-        self.accept("m", self.__handlePressMouse)
-        taskMgr.add(self.__cameraControlTask, "InteractiveCameraControlTask")
+        self.accept('m', self.__handlePressMouse)
+        self.accept('=' or '+', self.__handleCameraIn)
+        self.accept('-', self.__handleCameraOut)
+        self.accept('r', self.__resetCameraPos)
+        taskMgr.add(self.__cameraControlTask, 'InteractiveCameraControlTask')
+        taskMgr.add(self.__updateCanControl, 'UpdateCan_InteractiveCameraControlTask')
         
     def __handlePressMouse(self):
-        self.wantControl = not self.wantControl
-        if self.wantControl:
-            self.mouseCoords = [base.win.getPointer(0).getX(), base.win.getPointer(0).getY()]
-            props = WindowProperties()
-            props.setCursorHidden(True)
-            base.win.requestProperties(props)
+        if self.canControlCam:
+            self.wantControl = not self.wantControl
+            if self.wantControl:
+                self.mouseCoords = [base.win.getPointer(0).getX(), base.win.getPointer(0).getY()]
+                props = WindowProperties()
+                props.setCursorHidden(True)
+                base.win.requestProperties(props)
+            else:
+                props = WindowProperties()
+                props.setCursorHidden(False)
+                base.win.requestProperties(props)
+
+    def __resetCameraPos(self):
+        self.cameraParent.setPosHpr(0,0,0,0,0,0)
+
+    def __updateCanControl(self, task):
+        if self.target.getY(self.cameraParent) == self.MIN_CAM_DISTANCE:
+
+            if self.cameraParent.getHpr() == Vec3(0,0,0) and self.cameraParent.getPos() == Vec3(0,0,0):
+                self.canControlCam = True
+                self.canZoom = True
+            else:
+                self.canZoom = False
         else:
-            props = WindowProperties()
-            props.setCursorHidden(False)
-            base.win.requestProperties(props)
+            self.canControlCam = False
+
+        return Task.cont
+
+    def __handleCameraIn(self):
+        if self.cameraParent != None:
+
+            if self.canZoom:
+                if self.target.getY(self.cameraParent) > self.MIN_CAM_DISTANCE < self.MAX_CAM_DISTANCE:
+                    self.cameraParent.setY(self.cameraParent.getY() + 1.0)
+
+    def __handleCameraOut(self):
+        if self.cameraParent != None:
+
+            if self.canZoom:
+                if self.target.getY(self.cameraParent) < self.MAX_CAM_DISTANCE > self.MIN_CAM_DISTANCE:
+                    self.cameraParent.setY(self.cameraParent.getY() - 1.0)
         
     def stop(self):
-        self.ignore("m")
+        self.ignore('m')
+        self.ignore('=' and '+')
+        self.ignore('-')
+        self.ignore('r')
         self.wantControl = False
-        taskMgr.remove("InteractiveCameraControlTask")
+        self.canControlCam = False
+        self.canZoom = False
+        taskMgr.remove('InteractiveCameraControlTask')
+        taskMgr.remove('UpdateCan_InteractiveCameraControlTask')
         
     # todo: rotate avatar with camera change
     def __cameraControlTask(self, task):
-        if self.wantControl:
-            mouseD = base.win.getPointer(0)
-            x = mouseD.getX()
-            y = mouseD.getY()
-            
-            heading = self.cameraParent.getH()
-            pitch = self.cameraParent.getP()
-            if base.win.movePointer(0, self.mouseCoords[0], self.mouseCoords[1]):
-                heading -= (x - self.mouseCoords[0]) * CameraFactor
-                pitch -= (y - self.mouseCoords[1]) * CameraFactor
+        if self.canControlCam:
+            if self.wantControl:
+                mouseD = base.win.getPointer(0)
+                x = mouseD.getX()
+                y = mouseD.getY()
+                
+                heading = self.cameraParent.getH()
+                pitch = self.cameraParent.getP()
+                if base.win.movePointer(0, self.mouseCoords[0], self.mouseCoords[1]):
+                    heading -= (x - self.mouseCoords[0]) * CameraFactor
+                    pitch -= (y - self.mouseCoords[1]) * CameraFactor
 
-                if pitch > self.PITCH_LIMIT:
-                    pitch = self.PITCH_LIMIT
-                    
-            self.cameraParent.setHpr(heading, pitch, 0)
+                    if pitch > self.PITCH_LIMIT:
+                        pitch = self.PITCH_LIMIT
+                        
+                self.cameraParent.setHpr(heading, pitch, 0)
         
-        return task.cont
+        return Task.cont
