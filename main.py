36892053f1b0d1c0d0f021248e796dcf3017affa
CodeAnGo <luke@codeango.com>
2016-06-11 20:52:17 +0100

    Started Code Review 1.0
    Future Proofing

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import toontown.toonbase.Toontown2Start
+import runpy, os
+
+token = raw_input("Play Token:\n > ")
+password = raw_input("Password:\n > ") 
+
+if gameServer == "":
+	os.environ["GAMESERVER"] = "188.165.250.225"
+else:
+	os.environ["GAMESERVER"] = gameServer
+	
+os.environ["ACCOUNT_PLAYTOKEN"] = token
+os.environ["ACCOUNT_PASSWORD"] = password
+
+runpy.run_module('toontown.toonbase.Toontown2Start', run_name='__main__', alter_sys=True)
--- play_dubito.py
+++
@@ -1,4 +0,0 @@
-import os
-os.environ["GAMESERVER"] = "188.165.250.225"
-
-import main
