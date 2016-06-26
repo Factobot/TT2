6f14ff8d6703e19b79ffc224827089c02372fbcb
SkippsDev <SkippsDev@users.noreply.github.com>
2016-06-26 18:25:33 -0400

    Delete DistributedToonOV.py

--- toontown/toon/DistributedToonOV.py
+++
@@ -1,6 +0,0 @@
-from direct.distributed.DistributedObjectOV import DistributedObjectOV
-
-class DistributedToonOV(DistributedObjectOV):
-    
-    def __init__(self, air):
-        DistributedObjectOV.__init__(self, air)
