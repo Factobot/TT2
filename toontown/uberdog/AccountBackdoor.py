1ee7c19ae79cad2a09e2459abfb8a86b4c653ffc
SkippsDev <SkippsDev@users.noreply.github.com>
2016-06-28 22:22:17 -0400

    Added playtokens to the response

--- toontown/uberdog/AccountBackdoor.py
+++ toontown/uberdog/AccountBackdoor.py
@@ -57,7 +57,7 @@ class RequestHandler(BaseRequestHandler):
 
         if username in accounts:
             response = {
-                "type":"response-login_resp", "result":"access-granted", "reason": "Login sucess!"
+                "type":"response-login_resp", "result":"access-granted", "reason": "Login sucess!", str(username)
             }
 
             self.sendResponse(json.dumps(response))
